26d096991a703fc728e85a5f39a827feca476d68
Dmitriy Muntyan <muntyan.co@gmail.com>
2018-11-05 22:12:20 +0300

    Modify navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -105,17 +105,18 @@
     var navbar = $('#sticky-nav'),
         y_pos = navbar.offset().top,
         height = navbar.height();
+    
+    $('<div class="clone-nav"></div>').insertBefore(navbar).css('height', height).hide();
 
     $(document).scroll(function() {
         var scrollTop = $(this).scrollTop();
 
         if (scrollTop > y_pos) {
             navbar.addClass('fixed-top');
-            var shiftContent = navbar.height();
-            $('#block-2').css('margin-top', shiftContent + 'px');
+            $('.clone-nav').show();
         } else if (scrollTop <= y_pos) {
             navbar.removeClass('fixed-top');
-            $('#block-2').css('margin-top', '0px');
+            $('.clone-nav').hide();
         }
     });
     
