631a1efe001707696417c9eeba750360d3156594
Dmitriy Muntyan <muntyan.co@gmail.com>
2018-11-21 15:40:26 +0300

    Change block-2 on the main page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,6 +39,37 @@
 
   // Your custom JavaScript goes here
   jQuery(document).ready(function($) {
+
+    // jQuery smooth scrolling
+    $('a[href="#advantages-link"], a[href="#about-link"]')
+      .click(function(event) {
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top - 110
+            }, 1000, function() {
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { 
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); 
+                $target.focus();
+              };
+            });
+          }
+        }
+      });
+
+
+
     // Thumbnail on the product page
     $('.thumbnail').on('click', function() {
       var clicked = $(this);
